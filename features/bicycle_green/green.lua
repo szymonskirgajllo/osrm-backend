3db2a6177b614c2743c0ad6ccac68c1d617193ed
Emil Tin <emil.tin@tmf.kk.dk>
2014-08-22 11:26:00 +0200

    green profile using postgres

---
+++ features/bicycle_green/green.lua
@@ -0,0 +1,25 @@
+@routing @bicycle @area @green
+Feature: Bike - prioritize green routes through green areas
+
+	Background:
+		Given the profile "bicycle_green"
+	
+	@square	
+	Scenario: Bike - Prefer ways close to parks
+		Given the node map
+			|   |   | w | y |
+			| x | a | b |   |
+			|   | d | c |   |
+
+		And the ways
+			| nodes | area | amenity |
+			| xa    |      |         |
+            | ab    |      |         |
+            | by    |      |         |
+            | xw    |      |         |
+            | wy    |      |         |
+			| abcda | yes  | park    |
+		
+		When I route I should get
+			| from | to | route    |
+			| x    | y  | xa,ab,by |
